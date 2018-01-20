ad65f2d8bbf53c063765a9f286937ac2b76bd4d1
geblukasz <geblukasz@gmail.com>
2018-01-20 17:44:14 +0100

    dodanie generatora sumy kontrolnej

---
+++ src/GeneratorSumyMD5.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+
+public class GeneratorSumyMD5 {
+
+    static String getFileChecksum(MessageDigest digest, File plik)
+    {
+        //Przeczytaj plik
+        FileInputStream streamCzytaniaPliku = null;
+        try {
+            streamCzytaniaPliku = new FileInputStream(plik);
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie znaleziono pliku" + plik);
+            e.printStackTrace();
+        }
+
+        //Create byte array to read data in chunks
+        byte[] tablicaBajtow = new byte[1024];
+        int licznikBajtow = 0;
+
+        try {
+            while ((licznikBajtow = streamCzytaniaPliku.read(tablicaBajtow)) != -1) {
+                digest.update(tablicaBajtow, 0, licznikBajtow);
+            }
+        } catch (IOException e) {
+            System.out.println();
+            e.printStackTrace();
+        }
+
+        try {
+            streamCzytaniaPliku.close();
+        } catch (IOException e) {
+            System.out.println("Błąd podczas czytania pliku");
+            e.printStackTrace();
+        }
+
+        byte[] bytes = digest.digest();
+
+        //Przekonwertowanie z formatu decimal do hexDecimal
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i< bytes.length ;i++)
+        {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        //zwrocenie hash'a
+        return sb.toString();
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String checksum = null;
+
+        //Plik do obliczenia sumy kontrolnej
+        File plik = new File("C:\\Users\\Lukasz\\Desktop\\ProjektKlientSerwer\\WymianaPlikow\\PrzykladowyPlikDoTestow.txt");
+
+
+        //Algorytm MD5
+        MessageDigest md5Digest = null;
+        try {
+            md5Digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Błąd podczas pracy algorytmu");
+            e.printStackTrace();
+        }
+
+        //Obliczanie sumy kontrolnej
+        checksum = GeneratorSumyMD5.getFileChecksum(md5Digest, plik);
+
+        System.out.println("Suma kontrolna dla pliku " + plik + " wynosi: " + checksum);
+
     }
 }
